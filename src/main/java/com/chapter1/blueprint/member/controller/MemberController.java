847345c86fb618d0b785339bbe5c1504b42dbcba
kim jiwoo <annejwkim00@naver.com>
2024-11-03 02:13:12 +0900

    Reapply "Merge branch 'dev' into feature/myPage"
    This reverts commit 09c7ff9ad0d4b37a0615db1432cd0c1e386d6e9a.

--- src/main/java/com/chapter1/blueprint/member/controller/MemberController.java
+++ src/main/java/com/chapter1/blueprint/member/controller/MemberController.java
@@ -20,4 +20,8 @@ public class MemberController {
         memberService.updateMemberProfile(uid, inputProfileDTO);
         return ResponseEntity.ok("업데이트 성공");
     }
+    //@GetMapping(value = "/members/new")
+    //public String createForm() {
+    //    return "members/createMemberForm";
+    //}
 }
