110af26e7d00a511eb1ec5ee8317b1da54c58c98
JUNG DAYE <155501200+Jungdaye89@users.noreply.github.com>
2024-11-06 22:28:59 +0900

    Revert "Feat: subscription getAll 메서드 추가"

--- src/main/java/com/chapter1/blueprint/subscription/controller/SubscriptionController.java
+++ src/main/java/com/chapter1/blueprint/subscription/controller/SubscriptionController.java
@@ -1,7 +1,6 @@
 package com.chapter1.blueprint.subscription.controller;
 
 import com.chapter1.blueprint.exception.dto.SuccessResponse;
-import com.chapter1.blueprint.subscription.domain.SubscriptionList;
 import com.chapter1.blueprint.subscription.service.SubscriptionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -19,15 +16,9 @@ import java.util.List;
 public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
-    @GetMapping(value = "/update")
+    @GetMapping(value = "/update/subscription")
     public ResponseEntity<?> updateSubscription() {
         String result = subscriptionService.updateSub();
         return ResponseEntity.ok(new SuccessResponse(result));
-    }
-
-    @GetMapping(value = "/get")
-            public ResponseEntity<?> getSubscription() {
-        List<SubscriptionList> subscriptionLists = subscriptionService.getAllSubscription();
-        return ResponseEntity.ok(new SuccessResponse(subscriptionLists));
     }
 }
